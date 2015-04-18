64b08350fbaa740bbad82983697b599e9f7eb4be
Alex <alex1820m@gmail.com>
2015-04-17 19:24:04 -0500

    Cambios

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -77,14 +77,8 @@
 				<div class="row contenido">
 					<div class="col-sm-offset-3 col-sm-6">
 						<div class="row">
-							<div class="col-sm-3">
-								<img src="{{url()}}/img/hashtag.png" alt="" class="img-responsive pull-right hashtag">
-							</div>
-							<div class="col-sm-9">
-								<h1>
-									<strong>mamá</strong> <span>nos</span><br>
-									quiere <strong>cerca</strong>
-								</h1>
+							<div class="col-sm-12 center-block">
+								<img src="{{url()}}/img/titulo-home.png" alt="" class="img-responsive hashtag">
 							</div>
 						</div>
 						<br><br>
@@ -100,10 +94,7 @@
 						<div class="col-sm-10 col-sm-offset-1">
 							<div class="row">
 								<div class="col-sm-8 col-sm-offset-2 col-xs-12 titulo clearfix">
-									<img src="{{url()}}/img/hashtag.png" alt="" class="img-responsive">
-									<p>
-										<span class="home-sub-1">Las</span><br/><span class="home-sub-2">MEJORES</span><br/>madres
-									<p>
+									<img src="{{url()}}/img/titulo-hashtag.png" alt="" class="img-responsive">
 								</div>
 							</div>
 							<p class="parraf">
