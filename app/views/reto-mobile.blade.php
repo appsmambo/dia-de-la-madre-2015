1a6e7b0358f0c2bdd0898f8932f56f65104de286
Alex <alex1820m@gmail.com>
2015-04-21 13:12:59 -0500

    Cambios

--- app/views/reto-mobile.blade.php
+++ app/views/reto-mobile.blade.php
@@ -120,7 +120,7 @@ window.fbAsyncInit = function () {
 							<div class="kilos-reto">
 								<img src="{{url()}}/img/back-8-kilos.png" alt="" class="img-responsive center-block">
 								<p>
-									8kgs  <span>de ayuda</span>
+									12kgs  <span>de ayuda</span>
 								</p>
 							</div>
 						</div>
